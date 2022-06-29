52339f126f29a7e2add93a0a2af65c05ee76a6c3
MarieTagliavia <mtagliavia@uchicago.edu>
2022-06-29 15:38:53 -0500

    nicer formatting of analysis_funcs.py

--- analysis_funcs.py
+++ analysis_funcs.py
@@ -1,5 +1,7 @@
 '''
-This is where my graphing functions will go. I will import these functions into the documents I use to analyze the simulation data.
+This is where my graphing functions will go. I will import these functions into
+the documents I use to analyze the simulation data. This file includes the
+functions in graph_functions.py.
 '''
 
 import copy
@@ -8,14 +10,16 @@ import mplhep as hep
 import numpy as np
 
 
-def hist1(x, title, label, size=(10,10), nbins=25, xlabel=None, ylabel=None, xlim=(None,None), yscale='linear', loc='best'):
+def hist1(x, title, label, size=(10,10), nbins=25, xlabel=None, ylabel=None,
+                            xlim=(None,None), yscale='linear', loc='best'):
     '''
     This function produces a 1D histogram of the input data.
     
     Inputs:
         - x (1D array) : numpy array to be histogrammed
         - title (str)  : title of graph
-        - label (str)  : description of particles fired; appears in graph legend
+        - label (str)  : description of particles fired; appears in graph
+                         legend
     Kwargs:
         - size (tuple) : figure size of graph produced
         - nbins (int)  : number of bins in the histogram
@@ -23,7 +27,8 @@ def hist1(x, title, label, size=(10,10), nbins=25, xlabel=None, ylabel=None, xli
         - ylabel (str) : y-axis label
         - xlim (tuple) : range of x-values to display in the graph
         - yscale (str) : how to scale the values in each bin
-        - loc (str/int): location of the graph legend; 1 is upper right, 5 is middle right, 9 is top middle
+        - loc (str/int): location of the graph legend; 1 is upper right, 5 is
+                         middle right, 9 is top middle
         
     Returns:
         - an mplhep histogram plot
@@ -39,7 +44,8 @@ def hist1(x, title, label, size=(10,10), nbins=25, xlabel=None, ylabel=None, xli
     
     
     
-def scatter1(x, y, title, xlabel, ylabel, label, size=(10,10), s=1, xlim=(None,None), ylim=(None,None), yscale='linear', loc='best'):
+def scatter1(x, y, title, xlabel, ylabel, label, size=(10,10), s=1,
+            xlim=(None,None), ylim=(None,None), yscale='linear', loc='best'):
     '''
     This function produces a 2D scatterplot of the input data.
     
@@ -49,14 +55,16 @@ def scatter1(x, y, title, xlabel, ylabel, label, size=(10,10), s=1, xlim=(None,N
         - title (str)  : title of graph
         - xlabel (str) : x-axis label
         - ylabel (str) : y-axis label
-        - label (str)  : description of particles fired; appears in graph legend
+        - label (str)  : description of particles fired; appears in graph
+                         legend
     Kwargs:
         - size (tuple) : figure size of graph produced
         - s (float)    : size of points graphed
         - xlim (tuple) : range of x-values to display in the graph
         - ylim (tuple) : range of y-values to display in the graph
         - yscale (str) : how to scale the values in each bin
-        - loc (str/int): location of the graph legend; 1 is upper right, 5 is middle right, 9 is top middle
+        - loc (str/int): location of the graph legend; 1 is upper right, 5 is
+                         middle right, 9 is top middle
         
     Returns:
         - a matplotlib.pyplot scatterplot
@@ -74,24 +82,32 @@ def scatter1(x, y, title, xlabel, ylabel, label, size=(10,10), s=1, xlim=(None,N
     
     
     
-def hists(xs, title, labels, size=(10,10), nbins=25, alpha=0.6, colors=None, xlabel=None, ylabel=None, xlim=(None,None), yscale='linear', loc='best'):
+def hists(xs, title, labels, size=(10,10), nbins=25, alpha=0.6, colors=None,
+    xlabel=None, ylabel=None, xlim=(None,None), yscale='linear', loc='best'):
     '''
     This function graphs many histograms on the same plot.
     
     Inputs:
-        - xs (2D array)      : 2D numpy array where each element is one dataset to be histogrammed
+        - xs (2D array)      : 2D numpy array where each element is one
+                               dataset to be histogrammed
         - title (str)        : title of graph
-        - label (list)       : description of particles fired; appears in graph legend
+        - label (list)       : description of particles fired; appears in
+                               graph legend
     Kwargs:
         - size (tuple)       : figure size of graph produced
-        - nbins (int/array)  : number of bins in each histogram; can 'customize' for each individual histogram; can use output of calc_bins function
-        - alpha (float/array): opacity of histogram bars; can 'customize' for each individual histogram
-        - colors (array)     : color of each histogram; can 'customize' for each individual histogram
+        - nbins (int/array)  : number of bins in each histogram; can
+                               'customize' for each individual histogram; can
+                               use output of calc_bins function
+        - alpha (float/array): opacity of histogram bars; can 'customize' for
+                               each individual histogram
+        - colors (array)     : color of each histogram; can 'customize' for
+                               each individual histogram
         - xlabel (str)       : x-axis label
         - ylabel (str)       : y-axis label
         - xlim (tuple)       : range of x-values to display in the graph
         - yscale (str)       : how to scale the values in each bin
-        - loc (str/int)      : location of the graph legend; 1 is upper right, 5 is middle right, 9 is top middle
+        - loc (str/int)      : location of the graph legend; 1 is upper right,
+                               5 is middle right, 9 is top middle
         
     Returns:
         - a matplotlib.pyplot graph of many histograms
@@ -118,7 +134,8 @@ def hists(xs, title, labels, size=(10,10), nbins=25, alpha=0.6, colors=None, xla
     plt.figure(figsize=size)
     
     for i in range(len(xs)):
-        h = plt.hist(xs[i], bins=nb[i], label=labels[i], alpha=a[i], color=cs[i])
+        h = plt.hist(xs[i], bins=nb[i], label=labels[i], alpha=a[i],
+        color=cs[i])
     
     plt.title(title, size=20)
     plt.xlabel(xlabel, size=18)
@@ -130,9 +147,11 @@ def hists(xs, title, labels, size=(10,10), nbins=25, alpha=0.6, colors=None, xla
     
     
     
-def stats(x, label=None, misc=False, mins=False, maxs=False, alll=False, minlim=None, maxlim=None, nextlowest=0, nexthighest=0):
+def stats(x, label=None, misc=False, mins=False, maxs=False, alll=False,
+                    minlim=None, maxlim=None, nextlowest=0, nexthighest=0):
     '''
-    This function prints out important statistics that give a better sense of the distribution.
+    This function prints out important statistics that give a better sense of
+    the distribution.
     
     Inputs:
         - x (1D array)     : distribution to analyze
@@ -141,11 +160,16 @@ def stats(x, label=None, misc=False, mins=False, maxs=False, alll=False, minlim=
         - misc (bool)      : whether or not to print miscellaneous statistics
         - mins (bool)      : whether or not to print minimum statistics
         - maxs (bool)      : whether or not to print maximum statistics
-        - alll (bool)      : whether or not to print all statistics; overrides False assignments
-        - minlim (float)   : elements below this number are shown and investigated
-        - maxlim (float)   : elements above this number are shown and investigated
-        - nextlowest (int) : the (n+1)-lowest value in the distribution will be shown and enumerated
-        - nexthighest (int): the (n+1)-highest value in the distribution will be shown and enumerated
+        - alll (bool)      : whether or not to print all statistics; overrides
+                             False assignments of variables above
+        - minlim (float)   : elements below this number are shown and
+                             investigated
+        - maxlim (float)   : elements above this number are shown and
+                             investigated
+        - nextlowest (int) : the (n+1)-lowest value in the distribution will
+                             be shown and enumerated
+        - nexthighest (int): the (n+1)-highest value in the distribution will
+                             be shown and enumerated
         
     Prints:
         - Many strings containing statements about the distribution
@@ -170,14 +194,16 @@ def stats(x, label=None, misc=False, mins=False, maxs=False, alll=False, minlim=
         print('10th percentile of distribution: {}'.format(np.quantile(x, 0.1)))
         print()
         if minlim!=None:
-            print('Number of elements below {}: {}'.format(minlim, x[x<minlim].size))
-            print('Array of elements below {}: {}'.format(minlim, np.sort(x[x<minlim])[::-1]))
+            print('Number of elements below {}: {}'.format(minlim,
+                                                            x[x<minlim].size))
+            print('Array of elements below {}: {}'.format(minlim,
+                                                np.sort(x[x<minlim])[::-1]))
             print()
         if nextlowest!=0:
             xcp = copy.deepcopy(x)
             xcp = np.where(xcp==xcp.min(), xcp.max(), xcp)
             for i in range(nextlowest):
-                print('Next lowest value is: {}'.format(xcp.min()))  #HERE
+                print('Next lowest value is: {}'.format(xcp.min()))
                 print('Number of elements with this value: {}'.format(xcp[xcp==xcp.min()].size))
                 print()
                 xcp = np.where(xcp==xcp.min(), xcp.max(), xcp)
@@ -189,8 +215,10 @@ def stats(x, label=None, misc=False, mins=False, maxs=False, alll=False, minlim=
         print('90th percentile of distribution: {}'.format(np.quantile(x, 0.9)))
         print()
         if maxlim!=None:
-            print('Number of elements above {}: {}'.format(maxlim, x[x>maxlim].size))
-            print('Array of elements above {}: {}'.format(maxlim, np.sort(x[x>maxlim])))
+            print('Number of elements above {}: {}'.format(maxlim,
+                                                            x[x>maxlim].size))
+            print('Array of elements above {}: {}'.format(maxlim,
+                                                        np.sort(x[x>maxlim])))
             print()
         if nexthighest!=0:
             xcp = copy.deepcopy(x)
@@ -206,7 +234,8 @@ def stats(x, label=None, misc=False, mins=False, maxs=False, alll=False, minlim=
     
     
 '''
-These are the functions needed to graph the 2D scatterplot of binned medians for hit energies and hit positions
+These are the functions needed to graph the 2D scatterplot of binned medians
+for hit energies and hit positions.
 '''
 
 def calc_bins(a1, a2, nbins=15):
@@ -214,8 +243,10 @@ def calc_bins(a1, a2, nbins=15):
     Returns the bins to be used in the 2D scatterplot
     
     Inputs:
-        - a1 (numpy array): coordinate array for first neutron energy (e.g. 1 GeV)
-        - a2 (numpy array): coordinate array for second neutron energy (e.g. 2 GeV)
+        - a1 (numpy array): coordinate array for first neutron energy
+                            (e.g. 1 GeV)
+        - a2 (numpy array): coordinate array for second neutron energy
+                            (e.g. 2 GeV)
         - nbins (int): starting number of desired bins
         
     Outputs:
@@ -264,19 +295,19 @@ def ms_and_qs(xy, bins, low=0.16, high=0.84):
     (Can also calculate other quantile ranges)
     
     Inputs:
-        - xy (numpy array): aformentioned 2D array
-        - bins (numpy array): output from calc_bins function, the binning
-                              to be used in the future histogram
-        - low (float in [0,1]): lower percentile for quantile range
+        - xy (numpy array)     : aformentioned 2D array
+        - bins (numpy array)   : output from calc_bins function, the binning
+                                 to be used in the future histogram
+        - low (float in [0,1]) : lower percentile for quantile range
         - high (float in [0,1]): higher percentile for quantile range
                               
     Outputs:
         - medians (numpy array): 2D array containing bin medians of
                                  coordinates and energies
-        - q_low (numpy array): 2D array containing 16th quartile value of
-                               coordinates and energies
-        - q_high (numpy array): 2D array containing 84th quartile value of
-                                coordinates and energies
+        - q_low (numpy array)  : 2D array containing 16th quartile value of
+                                 coordinates and energies
+        - q_high (numpy array) : 2D array containing 84th quartile value of
+                                 coordinates and energies
     '''
     
     nb = bins.size
@@ -347,7 +378,8 @@ def ms_and_qs(xy, bins, low=0.16, high=0.84):
 
 
 
-def ms_qs_graph(medians, q_lows, q_highs, coord, energies, fs=12, ms=8, loc='best'):
+def ms_qs_graph(medians, q_lows, q_highs, coord, energies, fs=12, ms=8,
+                                                                loc='best'):
     '''
     Graphs the medians and quantiles as returned from the function
     ms_and_qs
@@ -370,7 +402,8 @@ def ms_qs_graph(medians, q_lows, q_highs, coord, energies, fs=12, ms=8, loc='bes
           bin
     '''
     
-    colors = plt.cm.Set1(np.linspace(0, 1, medians.shape[0]))  # change the color scheme here if you like
+    colors = plt.cm.Set1(np.linspace(0, 1, medians.shape[0]))  # change the
+                                                # color scheme here if you like
     
     plt.figure(figsize=(fs,fs))
     plt.title('Median Hit Energy vs Median {}-Coordinates'.format(coord.upper()))
@@ -378,9 +411,12 @@ def ms_qs_graph(medians, q_lows, q_highs, coord, energies, fs=12, ms=8, loc='bes
     plt.ylabel('median energy of hit (MeV)')
     
     for i in range(medians.shape[0]):
-        plt.errorbar(medians[i,:,0], medians[i,:,1], xerr=q_lows[i,:,0], yerr=q_lows[i,:,1], uplims=True, xuplims=True,
-                     marker='o', ms=ms, label='{} GeV'.format(energies[i]), lw=0, elinewidth=2, color=colors[i])
-        plt.errorbar(medians[i,:,0], medians[i,:,1], xerr=q_highs[i,:,0], yerr=q_highs[i,:,1], lolims=True, xlolims=True,
+        plt.errorbar(medians[i,:,0], medians[i,:,1], xerr=q_lows[i,:,0],
+                     yerr=q_lows[i,:,1], uplims=True, xuplims=True,
+                     marker='o', ms=ms, label='{} GeV'.format(energies[i]),
+                     lw=0, elinewidth=2, color=colors[i])
+        plt.errorbar(medians[i,:,0], medians[i,:,1], xerr=q_highs[i,:,0],
+                     yerr=q_highs[i,:,1], lolims=True, xlolims=True,
                      marker='o', ms=ms, lw=0, elinewidth=2, color=colors[i])
         
     plt.legend(loc=loc)
